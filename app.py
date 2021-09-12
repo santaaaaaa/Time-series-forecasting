a7ff4c18863b5ea3c179391e630df247e4dca82c
Santhosh <81555196+santaaaaaa@users.noreply.github.com>
2021-09-12 13:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ data = data.set_index('date')
 
 
 app = Flask(__name__)
-model = SARIMAX(data.availability, order=(13,0,9)).fit()
+model = SARIMAX(data.availability,order=(13,0,9),seasonal_order=(0,0,4,12),enforce_stationarity=False,enforce_invertibility=False).fit()
 
 @app.route('/')
 def input():
