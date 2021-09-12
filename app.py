cbe55fab50d9c82c34e87c506d1a61f52440b415
Santhosh <81555196+santaaaaaa@users.noreply.github.com>
2021-09-12 13:36:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,8 @@ df = np.round(df,0)
 avail_df = pd.DataFrame(df, columns=['Availability']) #Array ouput of imputer is converted in to a DataFrame
 data = data.assign(availability=avail_df['Availability']) #Then replacing the imputed column to our original dataframe
 data = data.set_index('date')
-
+data.index = pd.DatetimeIndex(data.index.values,
+                               freq=train.index.inferred_freq)
 
 app = Flask(__name__)
 model = SARIMAX(data.availability,order=(13,0,9),seasonal_order=(0,0,4,12),enforce_stationarity=False,enforce_invertibility=False).fit()
